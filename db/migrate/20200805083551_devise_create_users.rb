e46531e6b259f0fe04109be190caea1ab15d9f98
kkh456 <kkta0710@icloud.com>
2020-08-05 19:31:08 +0900

    family_name,first_name,birthday追加

--- db/migrate/20200805083551_devise_create_users.rb
+++ db/migrate/20200805083551_devise_create_users.rb
@@ -4,10 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false, default: ""
+      t.string :nickname,           null: false, 
       t.string :email,              null: false, default: ""
-      t.string :password,           null: false, default:""
+      t.string :password,           null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :family_name,        null: false
+      t.string :first_name,         null: false
+      t.string :family_name_kana,   null: false
+      t.string :first_name_kana,    null: false
+      t.date :birthday,             null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
