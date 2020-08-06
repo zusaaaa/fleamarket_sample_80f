6f4114b0921ffc561095c8499b6231427f527a89
kkh456 <kkta0710@icloud.com>
2020-08-06 11:32:06 +0900

    nameカラム　ストロングパラメーター記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 7..128
         
   validates :nickname, :password, :email, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday, :postal_code, :prefecture, :city, :house_number, :phone_number
 end
