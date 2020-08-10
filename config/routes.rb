b5800cd0b8dd0c63e59b90244876160260fd0303
kkh456 <kkta0710@icloud.com>
2020-08-10 18:26:05 +0900

    商品詳細始めます

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
   devise_scope :user do
     get 'address', to: 'users/ragistrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
