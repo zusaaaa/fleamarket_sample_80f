c39aad74965c5af1c163c8cf193fd0cc8c51181b
moemi2611 <67952276+moemi2611@users.noreply.github.com>
2020-08-13 12:08:12 +0900

    商品一覧表示 -2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,6 @@
 class ItemsController < ApplicationController
-  
   def index
     @products = Product.all
-    # @imagess = Image.includes(:product).order("created_at DESC")
     @images = Image.all
-    # @image = Image.find(params[:id])
   end
-
 end
