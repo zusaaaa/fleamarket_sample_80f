789be01ddc8fe9c36cec5b3246809093a78b3bc9
moemi2611 <67952276+moemi2611@users.noreply.github.com>
2020-08-13 11:16:38 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   
   def index
+    @products = Product.all
+    # @imagess = Image.includes(:product).order("created_at DESC")
+    @images = Image.all
+    # @image = Image.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-  # resources :items, only: [:index]
+  resources :items, only: [:index]
   resources :products, only: [:index, :new, :create]
   # 画像の表示
   # resources :images, only: [:show]
