9d5fbeb5fea280debe57cc47752204db8339525f
kkh456 <kkta0710@icloud.com>
2020-08-14 16:49:55 +0900

    打ち間違い

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,6 @@ class ItemsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:product_name, :product_explanation, :brand, :product_status_id, :shipping_method_id, :shipping_charge_id, :prefecture_id, :days_until_shipping_id, :price, :status, images_attributes: [:src])
-    marge(exhibitor_id: current_user.id)
+    merge(exhibitor_id: current_user.id)
   end
 end
