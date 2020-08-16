4228daa9968261ab0d57f545b6fd0a17c945459d
shi-ya418 <takuya.shiiya@gmail.com>
2020-08-17 05:39:51 +0900

    save

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,5 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory = 'fleamarket80f'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket80f'
   end
 end
