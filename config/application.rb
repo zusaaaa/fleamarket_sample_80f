13d57fb66b23c7cd15c7333dfc4520d8d74402e7
shi-ya418 <takuya.shiiya@gmail.com>
2020-08-19 10:29:41 +0900

    save

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module FleamarketSample80f
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
