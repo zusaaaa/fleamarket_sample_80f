003a041e6d70164aba4bbc1bf180fa245e5b2756
zusaaaa <obbyokamoto@gmail.com>
2020-08-22 19:53:20 +0900

    動き完成
    ## 変更点
    ・出品製品一覧表示を追加
    ・登録カード一覧を部分テンプレート化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @product = Product.where("user_id = ? and status = ?", current_user.id, "出品中" )
   end
 end
