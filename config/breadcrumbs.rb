88390157f2a4705981749dd53ccbbab37c7ce02f
shi-ya418 <takuya.shiiya@gmail.com>
2020-08-22 11:09:19 +0900

    save

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -5,6 +5,14 @@ end
 crumb :mypage do
   link "マイページ", "#"
 end
+crumb :card_new do
+  link "クレジットカード登録", new_card_path
+  parent :mypage
+end
+crumb :card_show do
+  link "クレジットカード確認", card_path
+  parent :card_new
+end
 
 crumb :product_post do
   link "商品出品", new_product_path
@@ -34,4 +42,8 @@ end
 crumb :user_address do
   link "新規登録（住所登録）", address_path
   parent :user_new
+end
+crumb :product_purchase do
+  link "購入確認", purchase_product_path
+  parent :product_show
 end
