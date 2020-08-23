a17dce6c7a91ae202b5ced4bf0fd9c8266832f51
seeeeeno <ittdhitfhhj864269@gmail.com>
2020-08-23 20:20:53 +0900

    不要な記述とファイルを削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,7 +8,6 @@ class FavoritesController < ApplicationController
     @product = Product.find(params[:product_id])
     @favorite = current_user.favorites.create(product_id: params[:product_id])
     @favorites = Favorite.where(product_id: params[:product_id])
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -16,6 +15,5 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.find_by(product_id: @product.id)
     @favorite.destroy
     @favorites = Favorite.where(product_id: params[:product_id])
-    # redirect_back(fallback_location: root_path)
   end
 end
