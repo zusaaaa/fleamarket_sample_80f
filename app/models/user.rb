41c9596c0db23c74e0b9fbf32b13f4e54392015e
seeeeeno <ittdhitfhhj864269@gmail.com>
2020-08-23 20:52:46 +0900

    スペルミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :cards, foreign_key: true
   has_many :products
   has_many :comments
-  many :favorites, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def already_favorited?(product)
     self.favorites.exists?(product_id: product.id)
