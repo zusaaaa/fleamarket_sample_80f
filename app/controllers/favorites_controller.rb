8d04140734160cea1629fa0f1f296459a54a28fa
seeeeeno <ittdhitfhhj864269@gmail.com>
2020-08-23 21:30:55 +0900

    before_actionの作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,22 @@
 class FavoritesController < ApplicationController
+  before_action :set_product, only: [:create, :destroy]
+  
   def favorite
     @product = Product.find(params[:id])
   end
 
   def create
-    @product = Product.find(params[:product_id])
     @favorite = current_user.favorites.create(product_id: params[:product_id])
     @favorites = Favorite.where(product_id: params[:product_id])
   end
 
   def destroy
-    @product = Product.find(params[:product_id])
     @favorite = current_user.favorites.find_by(product_id: @product.id)
     @favorite.destroy
     @favorites = Favorite.where(product_id: params[:product_id])
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
