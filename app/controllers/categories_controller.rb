fc54758463ef907c176025a1855e6b34c1a3c6f6
zusaaaa <obbyokamoto@gmail.com>
2020-08-26 00:56:55 +0900

    sider指摘点修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,7 @@ class CategoriesController < ApplicationController
   end
 
   private
+
   def set_category
     @category = Category.find(params[:id])
     if @category.has_children?
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,19 +4,17 @@ class Category < ApplicationRecord
   accepts_nested_attributes_for :products
 
   def set_products
-    #親カテゴリーの場合
+    # 親カテゴリーの場合
     if self.root?
       first_id = self.indirects.first.id
       last_id = self.indirects.last.id
       products = Product.where(category_id: first_id..last_id)
-      return products
 
-    #子カテゴリーの場合
+    # 子カテゴリーの場合
     elsif self.has_children?
       first_id = self.children.first.id
       last_id = self.children.last.id
       products = Product.where(category_id: first_id..last_id)
-      return products
 
     else
       return self.products
